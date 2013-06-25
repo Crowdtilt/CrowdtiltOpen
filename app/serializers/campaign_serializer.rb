73941cc3389feaac519e427d1a5f03e564b5fdaf
Dan Hansen & Maxwell Salzberg <pair+dan+maxwell@backerkit.com>
2013-06-25 13:55:56 -0700

    small tweaks to api, after feedback from Marc
    remove some CMS style stuff from the payments response, and include the
    reward ID and timestamps for payments
    
    put fields back into campaign
    
    actually include our changes

--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -1,4 +1,6 @@
 class CampaignSerializer < ActiveModel::Serializer
+  has_many :rewards
+
   attributes :id, :name, :goal_type, :goal_dollars, :goal_orders,  :expiration_date, :ct_campaign_id, :media_type,
              :main_image, :main_image_delete, :video_embed_id, :video_placeholder, :video_placeholder_delete,
              :contributor_reference, :progress_text, :primary_call_to_action_button, :primary_call_to_action_description,
@@ -8,11 +10,13 @@ class CampaignSerializer < ActiveModel::Serializer
              :payment_type, :fixed_payment_amount, :min_payment_amount, :apply_processing_fee,
              :stats_number_of_contributions, :stats_raised_amount, :stats_tilt_percent,
              :stats_unique_contributors, :published_flag, :collect_shipping, :production_flag,
-             :include_rewards, :reward_reference, :collect_additional_info, :additional_info_label, :url
-
-  has_many :rewards
+             :include_rewards, :reward_reference, :collect_additional_info, :additional_info_label, :url, :payments_uri
 
   def url
     campaign_home_url(object)
+  end
+
+  def payments_uri
+    api_campaign_payments_path(campaign_id: object.id)
   end
 end
