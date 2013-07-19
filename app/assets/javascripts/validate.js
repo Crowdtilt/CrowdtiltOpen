75393ac59ea6f45e05e301a3a8ded9d647727f23
Marc St Raymond <marc@msaint.com>
2013-07-19 13:09:03 -0700

    cleaned up validation files, added validation for additional info and shipping

--- app/assets/javascripts/validate.js
+++ app/assets/javascripts/validate.js
@@ -1,245 +1,276 @@
 $( document ).ready(function() {
 
-	// validate '/admin'
-	$("#admin_website_form").validate({
+  // validate '/admin'
+  $("#admin_website_form").validate({
 
-		// custom handler to call named function ""
-		submitHandler: function (form) {
+    // custom handler to call named function ""
+    submitHandler: function (form) {
       Selfstarter.admin.submitWebsiteForm(form);
-		},
+    },
 
-		// validate the previously selected element when the user clicks out
+    // validate the previously selected element when the user clicks out
     onfocusout: function(element) {
       $(element).valid();
     },
-	    // hide the loader when form is not valid
-		invalidHandler: function(event, validator) {
-			$(".loader").hide();
+
+    // hide the loader when form is not valid
+    invalidHandler: function(event, validator) {
+      $(".loader").hide();
     },
 
-		// validation rules
-		rules: {
-			"settings[site_name]": { required: true },
-			"settings[reply_to_email]": { required: true, email: true },
-			"settings[header_link_url]": { url: true },
-			"settings[tweet_text]": { maxlength: 120 },
-			"settings[facebook_app_id]": { digits: true },
-		},
-		// validation messages
-		messages: {
-			"settings[site_name]": "Please enter your site name",
-			"settings[reply_to_email]": {
-				required: "Please enter a reply to email address",
-				email: "hmm. That doesn't look like a valid email"
-			},
-			"settings[header_link_url]": {
-				url: "hmm. That doesn't look like a valid URL. ex: http://crowdtilt.com"
-			},
-			"settings[tweet_text]": {
-				maxlength: "Oops! Must be under 120 characters so we have room to include the link to your campaign"
-			},
-			"settings[facebook_app_id]": {
-				digits: "Your Facebook app ID should only consist of numbers"
-			}
-		}
+    // validation rules
+    rules: {
+      "settings[site_name]": { required: true },
+      "settings[reply_to_email]": { required: true, email: true },
+      "settings[header_link_url]": { url: true },
+      "settings[tweet_text]": { maxlength: 120 },
+      "settings[facebook_app_id]": { digits: true },
+    },
+    // validation messages
+    messages: {
+      "settings[site_name]": "Please enter your site name",
+      "settings[reply_to_email]": {
+        required: "Please enter a reply to email address",
+        email: "hmm. That doesn't look like a valid email"
+      },
+      "settings[header_link_url]": {
+        url: "hmm. That doesn't look like a valid URL. ex: http://crowdtilt.com"
+      },
+      "settings[tweet_text]": {
+        maxlength: "Oops! Must be under 120 characters so we have room to include the link to your campaign"
+      },
+      "settings[facebook_app_id]": {
+        digits: "Your Facebook app ID should only consist of numbers"
+      }
+    }
 
-	});
+  });
 
-	// validate '/admin/campaigns/_form'
+  // validate '/admin/campaigns/_form'
+  $("#admin_campaign_form").validate({
 
-	$("#admin_campaign_form").validate({
-
-		// custom handler to call named function ""
-		submitHandler: function (form) {
+    // custom handler to call named function ""
+    submitHandler: function (form) {
       Selfstarter.admin.submitCampaignForm(form);
-		},
+    },
 
-		// validate the previously selected element when the user clicks out
+    // validate the previously selected element when the user clicks out
     onfocusout: function(element) {
       $(element).valid();
     },
-	    // hide the loading spinner when form is invalid
-		invalidHandler: function(event, validator) {
-			$(".loader").hide();
+
+    // hide the loading spinner when form is invalid
+    invalidHandler: function(event, validator) {
+      $(".loader").hide();
     },
 
-		// validation rules
-		rules: {
-			"campaign[name]": { required: true },
-			"campaign[goal_dollars]": { required: true, number: true, min: 1 },
-			"campaign[goal_orders]": { required: true, digit: true, min: 1 },
-			"campaign[expiration_date]": { required: true, date: true },
-			"campaign[min_payment_amount]": { required: true, number: true, min: 1 },
-			"campaign[fixed_payment_amount]": { required: true, number: true, min: 1 },
-			"campaign[reward_reference]": { required: true },
-			"campaign[contributor_reference]": { required: true },
-			"campaign[video_embed_id]": { minlength: 11 , maxlength: 11},
-			"campaign[primary_call_to_action_button]": { required: true },
-			"campaign[secondary_call_to_action_button]": { required: true },
-			"campaign[tweet_text]": { maxlength: 120 }
-		},
-		// validation messages
-		messages: {
-			"campaign[name]": {
-				required: "You must pick a name for your campaign"
-			},
-			"campaign[goal_dollars]": {
-				required: "You must specify a goal dollar amount",
-				number: "Numbers only, please",
-				min: "Please enter a goal higher than $1"
-			},
-			"campaign[expiration_date]": {
-				required: "You must select a date from the calendar",
-				date: "Strange... that doesn't look like a valid date and time"
-			},
-			"campaign[min_payment_amount]": {
-				required: "You must specify a minimum payment amount",
-				number: "Numbers only, please",
-				min: "Please enter a minimum payment amount higher than $1"
-			},
-			"campaign[fixed_payment_amount]": {
-				required: "You must specify a fixed payment amount",
-				number: "Numbers only, please",
-				min: "Please enter a fixed payment amount higher than $1"
-			},
-			"campaign[reward_reference]": {
-				required: "You must choose a word"
-			},
-			"campaign[contributor_reference]": {
-				required: "You must choose a word"
-			},
-			"campaign[video_embed_id]": {
-				minlength: "Your Youtube ID is too short! <br> It should be 11 characters long.",
-				maxlength: "Oops. Don't include the whole link. <br> Only paste the last 11 characters of the URL."
-			},
-			"campaign[primary_call_to_action_button]": {
-				required: "You need some text here -- can't raise funds without it!"
-			},
-			"campaign[secondary_call_to_action_button]": {
-				required: "You need some text here -- can't raise funds without it!"
-			},
-			"campaign[tweet_text]": {
-				maxlength: "Oops! Must be under 120 characters so we have room to include the link to your campaign."
-			}
-		}
+    // validation rules
+    rules: {
+      "campaign[name]": { required: true },
+      "campaign[goal_dollars]": { required: true, number: true, min: 1 },
+      "campaign[goal_orders]": { required: true, digit: true, min: 1 },
+      "campaign[expiration_date]": { required: true, date: true },
+      "campaign[min_payment_amount]": { required: true, number: true, min: 1 },
+      "campaign[fixed_payment_amount]": { required: true, number: true, min: 1 },
+      "campaign[additional_info_label]": { required: true },
+      "campaign[reward_reference]": { required: true },
+      "campaign[contributor_reference]": { required: true },
+      "campaign[video_embed_id]": { minlength: 11 , maxlength: 11},
+      "campaign[primary_call_to_action_button]": { required: true },
+      "campaign[secondary_call_to_action_button]": { required: true },
+      "campaign[tweet_text]": { maxlength: 120 }
+    },
+    // validation messages
+    messages: {
+      "campaign[name]": {
+        required: "You must pick a name for your campaign"
+      },
+      "campaign[goal_dollars]": {
+        required: "You must specify a goal dollar amount",
+        number: "Numbers only, please",
+        min: "Please enter a goal higher than $1"
+      },
+      "campaign[expiration_date]": {
+        required: "You must select a date from the calendar",
+        date: "Strange... that doesn't look like a valid date and time"
+      },
+      "campaign[min_payment_amount]": {
+        required: "You must specify a minimum payment amount",
+        number: "Numbers only, please",
+        min: "Please enter a minimum payment amount higher than $1"
+      },
+      "campaign[fixed_payment_amount]": {
+        required: "You must specify a fixed payment amount",
+        number: "Numbers only, please",
+        min: "Please enter a fixed payment amount higher than $1"
+      },
+      "campaign[additional_info_label]": {
+        required: "Please describe what additional info you need!"
+      },
+      "campaign[reward_reference]": {
+        required: "You must choose a word"
+      },
+      "campaign[contributor_reference]": {
+        required: "You must choose a word"
+      },
+      "campaign[video_embed_id]": {
+        minlength: "Your Youtube ID is too short! <br> It should be 11 characters long.",
+        maxlength: "Oops. Don't include the whole link. <br> Only paste the last 11 characters of the URL."
+      },
+      "campaign[primary_call_to_action_button]": {
+        required: "You need some text here -- can't raise funds without it!"
+      },
+      "campaign[secondary_call_to_action_button]": {
+        required: "You need some text here -- can't raise funds without it!"
+      },
+      "campaign[tweet_text]": {
+        maxlength: "Oops! Must be under 120 characters so we have room to include the link to your campaign."
+      }
+    }
 
-	});
+  });
 
-	// validate '/admin/bank-setup'
-	$("#admin_bank_form").validate({
+  // validate '/admin/bank-setup'
+  $("#admin_bank_form").validate({
 
-		// custom handler to call named function ""
-		submitHandler: function (form) {
-			Selfstarter.admin.submitBankForm(form);
-		},
+    // custom handler to call named function ""
+    submitHandler: function (form) {
+      Selfstarter.admin.submitBankForm(form);
+    },
 
-		// validate the previously selected element when the user clicks out
+    // validate the previously selected element when the user clicks out
     onfocusout: function(element) {
       $(element).valid();
     },
-	    // hide the loader when form is not valid
-	    // add back in name attributes when form is not valid
-		invalidHandler: function(event, validator) {
-			$(".loader").hide();
+
+    // hide the loader when form is not valid
+    // add back in name attributes when form is not valid
+    invalidHandler: function(event, validator) {
+      $(".loader").hide();
       $('#bank_routing_number').attr('name', 'bank_routing_number');
       $('#account_number').attr('name', 'account_number');
     },
 
-		// validation rules
-		rules: {
-			full_legal_name: { required: true },
-			phone: { required: true, phoneUS: true },
-			street_address: { required: true },
-			zip: { required: true, number: true, minlength: 5 },
-			birth_year: { required: true, number: true, minlength: 4, maxlength: 4 },
-			bank_routing_number: { required: true, number: true, minlength: 9, usRoutingNumber: true },
-			account_number: { required: true }
-		},
-		// validation messages
-		messages: {
-			full_legal_name: {
-				required: "We need your full, legal name"
-			},
-			phone: {
-				required: "We need your phone number",
-				phoneUS: "Oops! That doesn't look valid"
-			},
-			street_address: {
-				required: "We need your street address"
-			},
-			zip: {
-				required: "We need your zip code",
-				number: "No letters in zip codes!",
-				minlength: "hmm. That doesn't <br>look like a zip code"
-			},
-			birth_year: {
-				required: "We need to know when you were born.",
-				number: "No letters in birth years!",
-				minlength: "Too few digits! You're not that old, are you?!",
-				maxlength: "Too many digits! Are you from the future?!"
-			},
-			bank_routing_number: {
-				required: "We need this to deposit funds<br> in your account.",
-				number: "No letters in the routing number!",
-				minlength: "US routing numbers are 9 digits",
-				usRoutingNumber: "hmm. That routing number <br> doesn't look valid."
-			},
-			account_number: {
-				required: "We need this to deposit funds<br> in your account."
-			}
-		}
+    // validation rules
+    rules: {
+      full_legal_name: { required: true },
+      phone: { required: true, phoneUS: true },
+      street_address: { required: true },
+      zip: { required: true, number: true, minlength: 5 },
+      birth_year: { required: true, number: true, minlength: 4, maxlength: 4 },
+      bank_routing_number: { required: true, number: true, minlength: 9, usRoutingNumber: true },
+      account_number: { required: true }
+    },
+    // validation messages
+    messages: {
+      full_legal_name: {
+        required: "We need your full, legal name"
+      },
+      phone: {
+        required: "We need your phone number",
+        phoneUS: "Oops! That doesn't look valid"
+      },
+      street_address: {
+        required: "We need your street address"
+      },
+      zip: {
+        required: "We need your zip code",
+        number: "No letters in zip codes!",
+        minlength: "hmm. That doesn't <br>look like a zip code"
+      },
+      birth_year: {
+        required: "We need to know when you were born.",
+        number: "No letters in birth years!",
+        minlength: "Too few digits! You're not that old, are you?!",
+        maxlength: "Too many digits! Are you from the future?!"
+      },
+      bank_routing_number: {
+        required: "We need this to deposit funds<br> in your account.",
+        number: "No letters in the routing number!",
+        minlength: "US routing numbers are 9 digits",
+        usRoutingNumber: "hmm. That routing number <br> doesn't look valid."
+      },
+      account_number: {
+        required: "We need this to deposit funds<br> in your account."
+      }
+    }
 
-	});
+  });
 
-	// validate '{campaign_name}/checkout/payment'
-	$("#payment_form").validate({
+  // validate '{campaign_name}/checkout/payment'
+  $("#payment_form").validate({
 
-		// custom handler to call named function "do_payment"
+    // custom handler to call named function "do_payment"
     submitHandler: function(form) {
       Selfstarter.campaigns.submitPaymentForm(form);
     },
 
-		// validate the previously selected element when the user clicks out
+    // validate the previously selected element when the user clicks out
     onfocusout: function(element) {
       $(element).valid();
     },
-	  // hide the loader when form is not valid
-	  // add back in name attributes when form is not valid
+
+    // hide the loader when form is not valid
+    // add back in name attributes when form is not valid
     invalidHandler: function(event, validator) {
       $(".loader").hide();
       $('#card_number').attr('name', 'card_number');
       $('#security_code').attr('name', 'security_code');
     },
 
-		// validation rules
-		rules: {
-			fullname: { required: true }, //insert + strip before submission of form
-			email: { required: true, email: true },
-			card_number: { required: true, minlength: 12, creditcard: true},
-			security_code: { required: true, number: true, minlength: 3, maxlength: 4 }
-		},
-		// validation messages
-		messages: {
-			fullname: {
-				required: "We need your full name"
-			},
-			email: {
-				required: "Please enter an email address",
-				email: "hmm. That doesn't look like a valid email"
-			},
-			card_number: {
-				required: "We need your card number to charge it",
-				minlength: "hmm. That doesn't look valid just yet",
-				creditcard: "hmm. That doesn't look valid just yet"
-			},
-			security_code: {
-				required: "We need the security code from your card",
-				number: "Numbers only for the security code!",
-				minlength: "Security codes are at least 3 digits!",
-				maxlength: "Security codes are 4 digits or less!"
-			}
-		}
+    // validation rules
+    rules: {
+      fullname: { required: true }, //insert + strip before submission of form
+      email: { required: true, email: true },
+      additional_info: { required: true },
+      address_one: { required: true },
+      city: { required: true },
+      state: { required: true },
+      postal_code: { required: true },
+      country: { required: true },
+      card_number: { required: true, minlength: 12, creditcard: true},
+      security_code: { required: true, number: true, minlength: 3, maxlength: 4 }
+    },
+    // validation messages
+    messages: {
+      fullname: {
+        required: "We need your full name"
+      },
+      email: {
+        required: "Please enter an email address",
+        email: "hmm. That doesn't look like a valid email"
+      },
+      additional_info: {
+        required: "We need the requested additional info"
+      },
+      address_one: {
+        required: "We need your street address"
+      },
+      city: {
+        required: "We need your city"
+      },
+      state: {
+        required: "We need your state"
+      },
+      postal_code: {
+        required: "We need your postal code"
+      },
+      country: {
+        required: "We need your country"
+      },
+      card_number: {
+        required: "We need your card number to charge it",
+        minlength: "hmm. That doesn't look valid just yet",
+        creditcard: "hmm. That doesn't look valid just yet"
+      },
+      security_code: {
+        required: "We need the security code from your card",
+        number: "Numbers only for the security code!",
+        minlength: "Security codes are at least 3 digits!",
+        maxlength: "Security codes are 4 digits or less!"
+      }
+    }
 
-	});
+  });
 
 });
