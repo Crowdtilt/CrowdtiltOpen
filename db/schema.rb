fffefbd29ebffb9a765496e4202b46c21420829f
Matt LeBel <matt.lebel@crowdtilt.com>
2013-08-30 15:42:02 -0700

    rebasing

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20130829221414) do
+ActiveRecord::Schema.define(:version => 20130830004146) do
 
   create_table "campaigns", :force => true do |t|
     t.string   "name"
