ab35577ea39a3c230eac98a36e8d1c2cee0772ad
Henry Liu <henry@liuhenry.com>
2013-09-09 14:59:45 -0700

    Enable cross subdomain session sharing

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Crowdhoster::Application.config.session_store :cookie_store, key: '_Crowdhoster_session'
+Crowdhoster::Application.config.session_store :cookie_store, key: '_Crowdhoster_session', :domain => :all, :tld_length => 2
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
