de5a10e680a2b0704b63d62a65efe91465c631a2
Henry Liu <henry@liuhenry.com>
2013-09-16 17:14:35 -0700

    Whitespace

--- app/validators/subdomain_validator.rb
+++ app/validators/subdomain_validator.rb
@@ -5,10 +5,10 @@ class SubdomainValidator < ActiveModel::EachValidator
     reserved_names = options[:reserved] if options[:reserved]
     if reserved_names.include?(value)
       record.errors[attribute] << 'cannot be a reserved name'
-    end                                              
-    
+    end
+
     record.errors[attribute] << 'must have between 3 and 63 letters' unless (3..63) === value.length
-    record.errors[attribute] << 'cannot start or end with a hyphen' unless value =~ /^[^-].*[^-]$/i                                                                                                    
+    record.errors[attribute] << 'cannot start or end with a hyphen' unless value =~ /^[^-].*[^-]$/i
     record.errors[attribute] << 'must be alphanumeric; A-Z, 0-9 or hyphen' unless value =~ /^[a-z0-9\-]*$/i
   end
 end
