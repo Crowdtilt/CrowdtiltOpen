ed27ad4ee1a8eda425d3ebf50049c8e0be7318cc
Henry Liu <henry@liuhenry.com>
2013-09-18 15:15:18 -0700

    Force secure session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Crowdhoster::Application.config.session_store :cookie_store, key: '_Crowdhoster_session', :domain => :all, :tld_length => 2
+Crowdhoster::Application.config.session_store :cookie_store, 
+  key: '_Crowdhoster_session', 
+  domain: :all, 
+  tld_length: 2,
+  secure: Rails.env.production?
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
