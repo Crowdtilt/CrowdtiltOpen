914b4ff57e78f1e325f2324c2f96f2cf27102106
Henry Liu <henry@liuhenry.com>
2013-10-09 14:59:30 -0700

    Fix redirect loop if user is not an admin and the campaign is not published

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   before_filter :check_init
 
   def index
-    if @settings.default_campaign
-      redirect_to campaign_home_url(@settings.default_campaign)
+    if @settings.default_campaign && ((user_signed_in? && current_user.admin?) || @settings.default_campaign.published_flag)
+        redirect_to campaign_home_url(@settings.default_campaign)
     else
       @campaigns = Campaign.order("created_at ASC")
       render 'theme/views/homepage'
