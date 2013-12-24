4f7ecbf51cf11d5c228204eba158c9ccee36f724
Matt LeBel <matt.lebel@crowdtilt.com>
2013-12-23 16:08:12 -0800

    add client and server validation for reward creation

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -2,6 +2,7 @@ class Reward < ActiveRecord::Base
   attr_accessible :title, :description, :delivery_date, :number, :price, :campaign_id, :visible_flag, :image_url
 
    validates :title, :description, :delivery_date, :price, presence: true
+   validates :image_url, :format => URI::regexp(%w(http https))
 
   belongs_to :campaign
   has_many :payments
