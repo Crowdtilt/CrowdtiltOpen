2456f423a69cbba14cbad9ed6ba6a89b91700fb7
rmostafa <ramy.mostafa@gmail.com>
2014-01-15 10:15:04 +0200

    Update railsScript.sh

--- railsScript.sh
+++ railsScript.sh
@@ -22,7 +22,7 @@ echo "----------   Installing Depend. & Imagemagick --------------"
 apt-get install libxslt-dev libxml2-dev libsqlite3-dev
 apt-get install imagemagick
 echo "----------   Installing Heroku --------------"
-echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
+echo "deb https://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
 wget -O- https://toolbelt.heroku.com/apt/release.key | apt-key add -
 apt-get update
 apt-get install heroku-toolbelt
