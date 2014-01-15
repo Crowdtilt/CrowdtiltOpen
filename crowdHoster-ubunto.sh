dfd9e4068962a5cadff5b61146d3f1e1fa06e6a5
rmostafa <ramy.mostafa@gmail.com>
2014-01-16 00:12:48 +0200

    Update crowdHoster-ubunto.sh

--- crowdHoster-ubunto.sh
+++ crowdHoster-ubunto.sh
@@ -11,7 +11,7 @@ echo "----------   Updating Sources List --------------"
 sed -i -e 's/us.archive.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list
 apt-get update
 echo "----------   Installing Curl --------------"
-apt-get -y install curl
+apt-get  install curl
 echo "----------   Installing RVM --------------"
 \curl -sSL https://get.rvm.io | bash
 echo "source /etc/profile.d/rvm.sh" >> ~/.bashrc
@@ -21,28 +21,28 @@ rvm requirements
 echo "----------   Setting RVM Installing Ruby 1.9.3 --------------"
 rvm install 1.9.3
 rvm use 1.9.3 --default
-apt-get -y install libyaml-dev
+apt-get install libyaml-dev
 gem update
 echo "----------   Setting RVM Installing Rails --------------"
 gem install --no-rdoc --no-ri rails
 echo "----------   Installing Depend. & Imagemagick --------------"
-apt-get -y install libxslt-dev libxml2-dev libsqlite3-dev
-apt-get -y install imagemagick
+apt-get  install libxslt-dev libxml2-dev libsqlite3-dev
+apt-get  install imagemagick
 echo "----------   Installing Heroku --------------"
 echo "deb https://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
 wget -O- https://toolbelt.heroku.com/apt/release.key | apt-key add -
 apt-get update
-apt-get -y install heroku-toolbelt
+apt-get  install heroku-toolbelt
 echo "----------   Installing Node JS --------------"
-apt-get -y install  python-software-properties python g++ make
+apt-get  install  python-software-properties python g++ make
 add-apt-repository ppa:chris-lea/node.js
 apt-get update
-apt-get -y install nodejs
+apt-get  install nodejs
 echo "----------   Installing GIT --------------"
-apt-get -y install git-core
+apt-get  install git-core
 echo "----------   Installing Postgresql --------------"
-apt-get -y install postgresql postgresql-contrib
-apt-get -y install libpq-dev
+apt-get  install postgresql postgresql-contrib
+apt-get  install libpq-dev
 rvmsudo gem install pg -v '0.17.1' -- --with-pg-lib=/usr/include/postgresql
 echo "------------ Database Configuration Started ----------"
 wget -O /etc/postgresql/9.1/main/pg_hba.conf https://raw2.github.com/rmostafa/Crowdhoster/master/pg_hba.conf
@@ -59,6 +59,7 @@ source /etc/profile.d/rvm.sh
 echo "----------   Updating With Bundle --------------"
 cd Crowdhoster
 cp .env.example .env
+bundle install
 echo "------------ Script Completed ----------"
 echo "Installation of Rails and CrowdHoster Configuration is Completed  !!"
 echo "------------ THANKS : CrowdHoster team ----------"
