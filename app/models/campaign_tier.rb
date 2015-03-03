31806a5ef4b8e5f9874788a69e952f766c681e13
Ken Koch <kkoch986@gmail.com>
2015-03-03 12:21:09 -0500

    round pct off and pct complete

--- app/models/campaign_tier.rb
+++ app/models/campaign_tier.rb
@@ -3,12 +3,12 @@ class CampaignTier < ActiveRecord::Base
   belongs_to :campaign
 
   def pct_off
-  	((self.campaign.base_price - self.price_at_tier) / self.campaign.base_price) * 100.0
+  	(((self.campaign.base_price - self.price_at_tier) / self.campaign.base_price) * 100.0).round(2)
   end
 
   def pct_complete
   	orders = self.campaign.orders
-  	(1.0 * [orders, self.min_users].min / self.min_users) * 100.0
+  	((1.0 * [orders, self.min_users].min / self.min_users) * 100.0).round(2)
   end
 
   def complete
